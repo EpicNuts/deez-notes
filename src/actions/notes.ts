60111f676a2311b86568b76f677a60850624b2ff
ben <benjamonstyles@gmail.com>
2025-04-02 18:19:28 +0200

    added openai. built a query, then connected the query to the Ask AI button

--- src/actions/notes.ts
+++ src/actions/notes.ts
@@ -3,6 +3,8 @@
 import { getUser } from "@/auth/server";
 import { prisma } from "@/db/prisma";
 import { handleError } from "@/lib/utils";
+import openai from "@/openai";
+import { ChatCompletionMessageParam, Completions } from "openai/resources/index.mjs";
 
 export const createNoteAction = async (noteId: string) => {
   try {
@@ -53,3 +55,57 @@ export const deleteNoteAction = async (noteId: string) => {
     return handleError(error)
   }
 };
+
+export const askAIAboutNotesAction = async (
+  newQuestions: string[], 
+  responses: string[]
+) => {
+    const user = await getUser();
+    if (!user) throw new Error("You must be logged in to query our robots");
+ 
+    const notes = await prisma.note.findMany({
+      where: {authorId: user.id},
+      orderBy: { createdAt: "desc" },
+      select: {text: true, createdAt: true, updatedAt: true },
+    });
+
+    if (notes.length === 0) {
+      return "You haven't written any notes yet."
+    }
+
+    const formattedNotes = notes
+      .map((note) =>
+      `
+      Text: ${note.text}
+      Created at: ${note.createdAt}
+      Last updated: ${note.updatedAt}
+      `.trim(),
+    )
+    .join("\n");
+
+    const messages: ChatCompletionMessageParam[] = [
+      {
+        role: "developer",
+        content: `
+        You are a helpful assistant that answers questions about the notes written by the user.
+        
+        Here are the user's notes:
+        ${formattedNotes}
+        `,
+      },
+    ];
+
+    for (let i=0; i < newQuestions.length; i++) {
+      messages.push({ role: "user", content: newQuestions[i] });
+      if (responses.length > i) {
+        messages.push({ role: "assistant", content: responses[i] });
+      }
+    }
+
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages
+    })
+
+    return completion.choices[0].message.content || "A problem has occurred";
+  }
