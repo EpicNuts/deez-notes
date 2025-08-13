a6c6f571b8d11d8050895f3a9fa94432c96c6bff
Ben Luck <benjamonstyles@protonmail.com>
2025-08-14 01:41:28 +0200

    added initial k6 load tests for the create-note and fetch-newest-note APIs

---
+++ k6/loadtests/create_note_loadtest.js
@@ -0,0 +1,27 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  projectID: __ENV.GRAFANA_PROJECT_ID || '3787225',
+  name: 'Create Note Load Test',
+  vus: 10,
+  duration: '30s',
+};
+
+function main() {
+  const BASE_URL = __ENV.BASE_URL || 'http://localhost:3000';
+  const userId = __ENV.LOAD_TEST_USERID;
+  const url = `${BASE_URL}/api/create-new-note?userId=${userId}`;
+  const payload = JSON.stringify({
+    title: 'Test Note',
+    content: 'This is a test note.'
+  });
+  const headers = { 'Content-Type': 'application/json' };
+
+
+  let res = http.post(url, payload, { headers });
+  check(res, { "status is 200": (res) => res.status === 200 || res.status === 201 });
+  sleep(1);
+}
+
+export default main;
